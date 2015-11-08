a00c4521113e5d1d4e685a7bbbde97d91383bb1b
Brian Tomlinson <darthlukan@gmail.com>
2015-11-08 14:05:13 +0100

    add an example of 'let over lambda' or lexical closure using a silly toy function that adds 5 to any number passed in

---
+++ let_over_lambda.scm
@@ -0,0 +1,23 @@
+#!/usr/bin/guile \
+-e main -s
+!#
+
+;; Number -> Number
+;; return Number y + 5
+;; Uses "let over lambda" or lexical closure.
+(define add5 
+          (let ((x 5)) 
+            (lambda (y)
+              (+ x y))))
+
+(define (main args)
+  ; Functional map for iterating over args
+  ; map takes a function as a handler and an iterable (cdr args)
+  ; where args is a list of args
+  (map 
+    (lambda (arg)
+      ; the shell passes args as strings, convert to number
+      ; NOTE: This will fail if an arg cannot be parsed as a number!
+      (display (add5 (string->number arg))))
+    (cdr args))
+  (newline))
