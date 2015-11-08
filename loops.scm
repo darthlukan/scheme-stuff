512cab5f901899eeb12597236ab64d7f669c96a7
Brian Tomlinson <darthlukan@gmail.com>
2015-11-08 19:09:24 +0100

    fix formatting in loops.scm

--- loops.scm
+++ loops.scm
@@ -3,19 +3,16 @@
 
 ;; Loops don't actually exist, but recursion does!
 ;; Calc factorial example
-(define factorial                           ;function name
-  (lambda (n)                               ;initial closure
-    (define internal                        ;internal var
-      (lambda (x current)                   ;function that actually performs work
-        (if (= x 0)                         ;base case
-          current                           ;return for base case
-          (internal (- x 1) (* x current))  ;else recurse
-          )
-        )
-      )
-    (internal n 1)))                        ;initial call
+(define factorial                               ;function name
+  (lambda (n)                                   ;initial closure
+    (define internal                            ;internal var
+      (lambda (x current)                       ;function that actually performs work
+        (if (= x 0)                             ;base case
+          current                               ;return for base case
+          (internal (- x 1) (* x current)))))   ;else recurse
+    (internal n 1)))                            ;initial call
 
-(display (factorial 5))                     ; => 120
+(display (factorial 5))                         ; => 120
 (newline)
 
 
@@ -24,14 +21,11 @@
   (lambda (i l)
     (cond ((null? l) #f)
           ((equal? i (car l)) #t)
-          (else (exists-in? i (cdr l)))
-    )
-  )
-)
+          (else (exists-in? i (cdr l))))))
 
 (define z (list "one" "two" "three"))
 
-(display (exists-in? "four" z))             ; => #f
+(display (exists-in? "four" z))                 ; => #f
 (newline)
-(display (exists-in? "two" z))              ; => #t
+(display (exists-in? "two" z))                  ; => #t
 (newline)
