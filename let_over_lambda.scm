1678403a97fdb9fba803bdb6f3b6cf071e83bf4d
Brian Tomlinson <darthlukan@gmail.com>
2025-04-15 23:27:51 -0700

    Clean up formatting

--- let_over_lambda.scm
+++ let_over_lambda.scm
@@ -5,19 +5,19 @@
 ;; Number -> Number
 ;; return Number y + 5
 ;; Uses "let over lambda" or lexical closure.
-(define add5 
-          (let ((x 5)) 
-            (lambda (y)
-              (+ x y))))
+(define add5
+  (let ((x 5))
+    (lambda (y)
+      (+ x y))))
 
 (define (main args)
-  ; Functional map for iterating over args
-  ; map takes a function as a handler and an iterable (cdr args)
-  ; where args is a list of args
+  ;; Functional map for iterating over args
+  ;; map takes a function as a handler and an iterable (cdr args)
+  ;; where args is a list of args
   (map 
-    (lambda (arg)
-      ; the shell passes args as strings, convert to number
-      ; NOTE: This will fail if an arg cannot be parsed as a number!
-      (display (add5 (string->number arg))) (display " "))
-    (cdr args))
+   (lambda (arg)
+     ;; the shell passes args as strings, convert to number
+     ;; NOTE: This will fail if an arg cannot be parsed as a number!
+     (display (add5 (string->number arg))) (display " "))
+   (cdr args))
   (newline))
